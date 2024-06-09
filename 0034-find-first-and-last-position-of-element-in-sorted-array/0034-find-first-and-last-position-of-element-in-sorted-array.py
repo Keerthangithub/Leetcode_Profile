de9c873b7f7d705fc09e20b13e112b51557fa55f
Keerthangithub <130146218+Keerthangithub@users.noreply.github.com>
2024-06-09 17:51:29 +0530

    Time: 75 ms (59.44%), Space: 17.7 MB (95.34%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -13,31 +13,5 @@ class Solution:
             s.append(s[0])
             return s
         if len(s)>=3:
-            l.append(s[0])
-            l.append(s[len(s)-1])
-            return l
+            return s[0],s[len(s)-1]
         return s
-        '''for i in range(len(nums)):
-            if len(nums)==1:
-                if nums[i]==target:
-                    return [i,i]
-                else:
-                    c=0
-            if len(nums)==2:
-                if nums[0]==nums[1] and nums[i]==target:
-                    return [0,1]
-                if nums[0]!=nums[1] and nums[i]==target:
-                    return [i,i]
-                else:
-                    c=0
-            if nums[i]==target:
-                c=1
-                s.append(i)
-            if len(s)==2:
-                break
-            if nums[i]!=target:
-                c=0
-        if c==0:
-            return [-1,-1]
-        else:
-            return s'''
