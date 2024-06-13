49ee3f7415128ef85fbc765f277e0dfe815c9eb2
Keerthangithub <130146218+Keerthangithub@users.noreply.github.com>
2024-06-13 11:05:36 +0530

    Time: 196 ms (37.23%), Space: 16.5 MB (97.07%) - LeetHub

--- 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.py
@@ -9,3 +9,17 @@ class Solution:
             k.append(c)
             c=0
         return k
+
+        '''d={}
+        for i in nums:
+            if i in d:
+                d[i]+=1
+            else:
+                d[i]=1
+        for i,j in d.items():
+            for x,y in d.items():
+                if x<i:
+                    c+=y
+            k.append(c)
+            c=0
+        return k'''
