6302e4b50fd909dbec0920c7f0b3a9432630775f
Keerthangithub <130146218+Keerthangithub@users.noreply.github.com>
2024-06-15 14:57:00 +0530

    Time: 40 ms (96.58%), Space: 16.5 MB (95.79%) - LeetHub

---
+++ 3397-find-the-integer-added-to-array-i/3397-find-the-integer-added-to-array-i.py
@@ -0,0 +1,10 @@
+class Solution:
+    def addedInteger(self, nums1: List[int], nums2: List[int]) -> int:
+        nums1.sort()
+        nums2.sort()
+        k=[]
+        for i in range(len(nums1)):
+            k.append(nums2[i]-nums1[i])
+        if k==[]:
+            return 0
+        return k[0]
