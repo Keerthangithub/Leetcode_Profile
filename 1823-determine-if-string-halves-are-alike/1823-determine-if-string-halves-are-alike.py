07fe5f5da1cc6ed961100c7323cdb6a93cc41720
Keerthangithub <130146218+Keerthangithub@users.noreply.github.com>
2024-07-04 15:56:30 +0530

    Time: 65 ms (5.47%), Space: 16.6 MB (14.41%) - LeetHub

--- 1823-determine-if-string-halves-are-alike/1823-determine-if-string-halves-are-alike.py
+++ 1823-determine-if-string-halves-are-alike/1823-determine-if-string-halves-are-alike.py
@@ -1,12 +1,10 @@
 class Solution:
     def halvesAreAlike(self, s: str) -> bool:
-        k=len(s)//2
         c,d=0,0
-        for i in range(k):
-            j=i+k
+        for i in range(len(s)//2):
             if s[i]=='a' or s[i]=='e' or s[i]=='i' or s[i]=='o' or s[i]=='u' or s[i]=='A' or s[i]=='E' or s[i]=='I' or s[i]=='O' or s[i]=='U':
                 c+=1
-            if s[j]=='a' or s[j]=='e' or s[j]=='i' or s[j]=='o' or s[j]=='u' or s[j]=='A' or s[j]=='E' or s[j]=='I' or s[j]=='O' or s[j]=='U':
+            if s[i+(len(s)//2)]=='a' or s[i+(len(s)//2)]=='e' or s[i+(len(s)//2)]=='i' or s[i+(len(s)//2)]=='o' or s[i+(len(s)//2)]=='u' or s[i+(len(s)//2)]=='A' or s[i+(len(s)//2)]=='E' or s[i+(len(s)//2)]=='I' or s[i+(len(s)//2)]=='O' or s[i+(len(s)//2)]=='U':
                 d+=1
         if c==d:
             return True
