22f12eeeaf80c34700d4400fa1cd4092533cdcdb
Keerthangithub <130146218+Keerthangithub@users.noreply.github.com>
2024-07-15 14:16:21 +0530

    Time: 44 ms (83.77%), Space: 16.7 MB (36.88%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.py
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.py
@@ -1,6 +1,5 @@
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
-        c=0
         for i in range(len(prices)):
             for j in range(i+1,len(prices)):
                 if prices[j]<=prices[i]:
